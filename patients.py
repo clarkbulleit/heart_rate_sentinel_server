920d04ccb88c2649bf272ff73f2b2a357c768096
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-13 16:19:01 -0500

    removed if name statement originally used for testing

--- patients.py
+++ patients.py
@@ -1,4 +1,3 @@
-from flask import jsonify
 from pymodm import connect
 from pymodm import MongoModel, fields
 
@@ -6,13 +5,13 @@ connect("mongodb://clarkbulleit:goduke112@ds037778.mlab.com:37778/patients_cb329
 
 
 class Patient(MongoModel):
-    patient_id = fields.IntegerField(primary_key=True)
+    patient_id = fields.IntegerField()
     attending_email = fields.EmailField()
     user_age = fields.IntegerField()
     heart_rate = fields.ListField()
     time = fields.DateTimeField()
 
 
-if __name__ == "__main__":
-    patient = Patient.objects.raw({"_id": 1}).first()
-    print(patient.user_age)
+# if __name__ == "__main__":
+    # patient = Patient.objects.raw({"_id": 1}).first()
+    # print(patient.user_age)
