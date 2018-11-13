3ac678fed5d6618011bea80d7dc9ac7bb97129cd
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-13 16:20:58 -0500

    added functions to the server file to test

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify, request
+from patients import Patient
+from pymodm import connect
 app = Flask(__name__)
 
 
 @app.route("/api/new_patient", methods=["POST"])
 def new_patient():
-    return
+    r = request.get_json()
+
+    p = Patient(patient_id=r['patient_id'], attending_email=r['attending_email'], user_age=r['user_age'])
+
+    p.save()
+
+    result = {
+        "message": "Added user {0} successfully to the class list".format(request.json["patient_id"])
+    }
+    return jsonify(result)
 
 
 @app.route("/api/heart_rate", methods=["POST"])
@@ -33,4 +44,5 @@ def int_average_hr(patient_id):
 
 
 if __name__ == "__main__":
+    connect("mongodb://clarkbulleit:goduke112@ds037778.mlab.com:37778/patients_cb329")
     app.run(host="127.0.0.1", port=5002)
