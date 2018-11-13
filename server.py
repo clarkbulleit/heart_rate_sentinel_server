0821e827cae91a2af5ce6f491bd6da8d3e9ec87f
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-13 13:47:26 -0500

    pep8 changes

--- server.py
+++ server.py
@@ -2,18 +2,20 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-
 @app.route("/api/new_patient", methods=["POST"])
 def new_patient():
     return
+
 
 @app.route("/api/heart_rate", methods=["POST"])
 def post_heart_rate():
     return
 
+
 @app.route("/api/status/<patient_id>", methods=["GET"])
 def status(patient_id):
     return
+
 
 @app.route("/api/heart_rate/<patient_id>", methods=["GET"])
 def get_heart_rate(patient_id):
@@ -22,6 +24,7 @@ def get_heart_rate(patient_id):
 @app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
 def average_heart_rate(patient_id):
     return
+
 
 @app.route("/api/heart_rate/interval_average", methods=["GET"])
 def int_average_hr(patient_id):
