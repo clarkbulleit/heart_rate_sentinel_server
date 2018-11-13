603e203fb449ed3a2bd005b8b2d952d97c84bca3
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-13 15:28:05 -0500

    changed url so that it could connect to mongodB

--- patients.py
+++ patients.py
@@ -1,7 +1,8 @@
+from flask import jsonify
 from pymodm import connect
 from pymodm import MongoModel, fields
 
-connect("mongodb://<dbuser>:<dbpassword>@ds037778.mlab.com:37778/patients_cb329")
+connect("mongodb://clarkbulleit:goduke112@ds037778.mlab.com:37778/patients_cb329")
 
 
 class Patient(MongoModel):
@@ -9,5 +10,9 @@ class Patient(MongoModel):
     attending_email = fields.EmailField()
     user_age = fields.IntegerField()
     heart_rate = fields.ListField()
+    time = fields.DateTimeField()
 
 
+if __name__ == "__main__":
+    patient = Patient.objects.raw({"_id": 1}).first()
+    print(patient.user_age)
