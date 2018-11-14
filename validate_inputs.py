201a2ee79e3b88eb9a707d9278cf7ee057f917ab
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-13 21:42:09 -0500

    removed custom error in favor or pre-existing KeyError

--- validate_inputs.py
+++ validate_inputs.py
@@ -1,13 +1,8 @@
 
-class PatientKeyError(Exception):
-    def __init__(self, message):
-        self.message = message
-
-
 def validate_inputs(r):
     patient_keys = [
         "patient_id", "attending_email", "user_age"
     ]
     for key in patient_keys:
         if key not in r.keys():
-            raise PatientKeyError("Key '{0}' not present in request".format(key))
+            raise KeyError
