8a574e73bdfdf7f89f95ffb0abbeccf7a0d65a0e
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-13 20:49:13 -0500

    added patient_id as primary key to remove duplicates

--- patients.py
+++ patients.py
@@ -5,7 +5,7 @@ connect("mongodb://clarkbulleit:goduke112@ds037778.mlab.com:37778/patients_cb329
 
 
 class Patient(MongoModel):
-    patient_id = fields.IntegerField()
+    patient_id = fields.IntegerField(primary_key=True)
     attending_email = fields.EmailField()
     user_age = fields.IntegerField()
     heart_rate = fields.ListField()
