e042b6bd2d2539ce0c9391addcd9e575d4ab601e
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-13 21:46:51 -0500

    added pytest.raisesn functionality to test that validate_inputs throws correct error

--- test_validate_inputs.py
+++ test_validate_inputs.py
@@ -1,6 +1,11 @@
 
-def test_validate_inputs(r, expected):
+def test_validate_inputs():
     import pytest
-    from validate_inputs import validate_inputs,PatientKeyError
+    from validate_inputs import validate_inputs
+
+    bad_input1 = {"patient_id": 1, "attending_email": 1}
+
+    with pytest.raises(KeyError):
+        validate_inputs(bad_input1)
 
 
