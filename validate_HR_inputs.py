0b29d7f0c60ed30cefc8a124f0a940f2de56fdc7
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-14 21:10:06 -0500

    added function to validate the hr input postings

---
+++ validate_HR_inputs.py
@@ -0,0 +1,10 @@
+
+def validate_hr_inputs(r):
+    hr_keys = [
+        "patient_id",
+        "heart_rate",
+    ]
+
+    for key in hr_keys:
+        if key not in r.keys() or len(r.keys()) != 3:
+            raise KeyError
