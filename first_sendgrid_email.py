9c3f53d85d928f49d2f1bb7910136d5313b1e7f3
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-15 16:28:19 -0500

    pep8 changes

--- first_sendgrid_email.py
+++ first_sendgrid_email.py
@@ -7,11 +7,13 @@ from sendgrid.helpers.mail import *
 
 def send_email(patient_id, timestamp):
     sg = sendgrid.SendGridAPIClient(apikey='SG.lpj1iTqMRtmDXd5GCxoiHQ.'
-                                           'rXDJvAce_fNivEwZMKC53uIE9Quq-ocTHAX9wgUAwzg')
+                                           'rXDJvAce_fNivEwZMKC53uIE9'
+                                           'Quq-ocTHAX9wgUAwzg')
     from_email = Email("clark.bulleit@duke.edu")
     to_email = Email("clarkbulleit@gmail.com")
     subject = "Patient {} is tachycardic".format(patient_id)
-    content = Content("text/plain", "Patient is tachycardic at {}".format(timestamp))
+    content = Content("text/plain", "Patient is tachycardic at {}"
+                      .format(timestamp))
     mail = Mail(from_email, subject, to_email, content)
     response = sg.client.mail.send.post(request_body=mail.get())
     print(response.status_code)
