dcad9a68bbf04eaebed40a62fc4f551952b347af
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-14 21:17:34 -0500

    added test cases to make sure this post command could only accept the keys patient_id and heart_rate

--- test_validate_hr_inputs.py
+++ test_validate_hr_inputs.py
@@ -3,6 +3,9 @@ import pytest
 
 @pytest.mark.parametrize("a", [
     {"patient_id": 1, "attending_email": 1},
+    {"patient_id": 1, "attending_email": 1, "user_age": 1, "heart_rate": 1},
+    {"patient_id": 1, "patient_id": 1},
+    {"patient_id": 1},
 ])
 def test_validate_hr_inputs(a):
     from validate_HR_inputs import validate_hr_inputs
