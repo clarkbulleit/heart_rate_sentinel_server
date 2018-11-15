33c4058db1485e64e4ac082445854a3b3d7c37b6
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-15 14:35:48 -0500

    added test cases to is_tachycardic testing function

--- test_is_tachycardic.py
+++ test_is_tachycardic.py
@@ -3,7 +3,10 @@ import pytest
 
 @pytest.mark.parametrize("a, b, expected", [
     (160, 12, True),
-
+    (138, 4, True),
+    (135, 5, True),
+    (129, 5, False),
+    (70, 40, False)
 ])
 def test_is_tachycardic(a, b, expected):
     from is_tachycardic import is_tachycardic
