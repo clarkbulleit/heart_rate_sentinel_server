63fb9a6abfdddc5c67a6827eb615b542cd41b8ad
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-14 19:17:08 -0500

    changed time from a datetimefield to a list field

--- client.py
+++ client.py
@@ -3,8 +3,10 @@ import requests
 if __name__ == "__main__":
     patient = {
         "patient_id": 1,
-        "attending_email": 'clarkbulleit@gmail.com',
-        "user_age": 55,
+        # "attending_email": 'clarkbulleit@gmail.com',
+        # "user_age": 55,
+        "heart_rate": 10,
     }
 
-    r = requests.post("http://127.0.0.1:5002/api/new_patient", json=patient)
+    # r = requests.post("http://127.0.0.1:5002/api/new_patient", json=patient)
+    r = requests.post("http://127.0.0.1:5002/api/heart_rate", json=patient)
