1b2e5cccd43a81262f235b48fc91ee9106fc9901
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-15 16:26:58 -0500

    changes sengrid test email script to a function that can be called in the server file

--- first_sendgrid_email.py
+++ first_sendgrid_email.py
@@ -4,14 +4,16 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-sg = sendgrid.SendGridAPIClient(apikey='SG.lpj1iTqMRtmDXd5GCxoiHQ.'
-                                       'rXDJvAce_fNivEwZMKC53uIE9Quq-ocTHAX9wgUAwzg')
-from_email = Email("clarkbulleit@gmail.com")
-to_email = Email("clark.bulleit@duke.edu")
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, subject, to_email, content)
-response = sg.client.mail.send.post(request_body=mail.get())
-print(response.status_code)
-print(response.body)
-print(response.headers)
+
+def send_email(patient_id, timestamp):
+    sg = sendgrid.SendGridAPIClient(apikey='SG.lpj1iTqMRtmDXd5GCxoiHQ.'
+                                           'rXDJvAce_fNivEwZMKC53uIE9Quq-ocTHAX9wgUAwzg')
+    from_email = Email("clark.bulleit@duke.edu")
+    to_email = Email("clarkbulleit@gmail.com")
+    subject = "Patient {} is tachycardic".format(patient_id)
+    content = Content("text/plain", "Patient is tachycardic at {}".format(timestamp))
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
