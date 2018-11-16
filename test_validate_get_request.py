4bf4c2192158cfe78528285d4f93e1e95a673086
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-15 19:41:48 -0500

    created function to test the validate_get_requests functions

--- test_validate_get_request.py
+++ test_validate_get_request.py
@@ -2,7 +2,11 @@ import pytest
 from validate_GET_request import validate_get_request
 
 
-def test_validate_get_request():
-    patient_id = 1
+@pytest.mark.parametrize("a,expected", [
+    ('a', 0),
+    (7, 1),
+    (5, 2)
+])
+def test_validate_get_request(a, expected):
 
-    assert validate_get_request(patient_id) == ([140], 1)
+    assert validate_get_request(a) == expected
