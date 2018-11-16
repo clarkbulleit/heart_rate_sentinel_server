2bffec0add2df60eb23ccd759c37175df02a2138
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-16 12:01:50 -0500

    allows client to hit vm

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import requests
 
+url = 'http://vcm-7335.vm.duke.edu:5002/'
+# url = 'http://127.0.0.1:5002/'
+
 
 def client_add_new_patient(id, email, age):
     patient = {
@@ -8,7 +11,7 @@ def client_add_new_patient(id, email, age):
         "attending_email": str(email),
         "user_age": int(age),
     }
-    r = requests.post("http://127.0.0.1:5002/api/new_patient",
+    r = requests.post(url + 'api/new_patient',
                       json=patient)
     return r
 
@@ -18,27 +21,24 @@ def client_add_patient_hr(id, hr):
         "patient_id": id,
         "heart_rate": hr,
     }
-    r = requests.post("http://127.0.0.1:5002/api/heart_rate",
+    r = requests.post(url + 'api/heart_rate',
                       json=patient)
     return r
 
 
 def client_get_status(patient_id):
-    url = 'http://127.0.0.1:5002/api/status/{}'.format(patient_id)
-    r = requests.get(url)
+    r = requests.get(url + 'api/status/{}'.format(patient_id))
     return r
 
 
 def client_get_hr_data(patient_id):
-    url = 'http://127.0.0.1:5002/api/heart_rate/{}'.format(patient_id)
-    r = requests.get(url)
+    r = requests.get(url + 'api/heart_rate/{}'.format(patient_id))
 
     return r
 
 
 def client_get_hr_avg(patient_id):
-    url = 'http://127.0.0.1:5002/api/heart_rate/average/{}'.format(patient_id)
-    r = requests.get(url)
+    r = requests.get(url + 'api/heart_rate/average/{}'.format(patient_id))
 
     return r
 
@@ -49,7 +49,7 @@ def client_post_interval(id, cutoff_time):
         "heart_rate_average_since": cutoff_time,
     }
 
-    r = requests.post("http://127.0.0.1:5002/api/heart_rate/interval_average",
+    r = requests.post(url + 'api/heart_rate/interval_average',
                       json=patient)
 
     return r
