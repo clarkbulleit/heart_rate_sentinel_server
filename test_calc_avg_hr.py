67debd841c3658fc07a51e50a5251e4640a66947
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-15 23:44:49 -0500

    added test cases (closes #20)

--- test_calc_avg_hr.py
+++ test_calc_avg_hr.py
@@ -1,10 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("a,b,expected", [
-    ([1, 2, 3, 4], 1, 10/4),
-    ([1, 1, 2], 1, 4/3),
+@pytest.mark.parametrize("a,b,c,expected", [
+    ([1, 2, 3, 4], 1, 1, 10/4),
+    ([1, 1, 2], 1, 1, 4/3),
 ])
-def test_calc_avg_hr(a, b, expected):
+def test_calc_avg_hr(a, b, c, expected):
     from calc_avg_hr import calc_avg_hr
-    assert calc_avg_hr(a, b) == expected
+    assert calc_avg_hr(a, b, c) == expected
