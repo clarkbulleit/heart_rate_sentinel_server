aaca6f397ef88d642ce07f3f760289dc0a30a84c
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-15 20:01:39 -0500

    added new test cases to the testing function for validate get requests

--- test_validate_get_request.py
+++ test_validate_get_request.py
@@ -4,8 +4,10 @@ from validate_GET_request import validate_get_request
 
 @pytest.mark.parametrize("a,expected", [
     ('a', 0),
-    (7, 1),
-    (5, 2)
+    (10000, 1),
+    (5, 2),
+    (1, 3),
+    (2, 3),
 ])
 def test_validate_get_request(a, expected):
 
