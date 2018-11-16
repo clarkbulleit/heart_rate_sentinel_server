16da5db1de1b96c84ec6368b023f72384e6f64a0
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-15 23:43:25 -0500

    allowed function that calculates heart rate to receive a cutoff time and a time vector

--- calc_avg_hr.py
+++ calc_avg_hr.py
@@ -1,6 +1,15 @@
+import datetime
 
 
-def calc_avg_hr(hr, date):
-
-    if date == 1:
-        return sum(hr)/len(hr)
+def calc_avg_hr(hr, times, cutoff_time):
+    hr_since = []
+    if cutoff_time == 1:
+        avg = sum(hr)/len(hr)
+    else:
+        for x in times:
+            if datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f') \
+                    > cutoff_time:
+                i = times.index(x)
+                hr_since.append(hr[i])
+        avg = sum(hr_since)/len(hr_since)
+    return avg
