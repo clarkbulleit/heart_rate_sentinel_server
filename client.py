299f94db93cafa32c3aff3ff763440280218e63a
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-15 22:32:17 -0500

    pep8 changes (closes #18)

--- client.py
+++ client.py
@@ -2,11 +2,11 @@ import requests
 
 if __name__ == "__main__":
     patient = {
-        "patient_id": 7,
-        # "attending_email": 'clarkbulleit@gmail.com',
-        # "user_age": 60,
-        "heart_rate": 105,
+        "patient_id": 1,
+        "attending_email": 'clarkbulleit@gmail.com',
+        "user_age": 55,
+        # "heart_rate": 105,
     }
 
-    # r = requests.post("http://127.0.0.1:5002/api/new_patient", json=patient)
-    r = requests.post("http://127.0.0.1:5002/api/heart_rate", json=patient)
+    r = requests.post("http://127.0.0.1:5002/api/new_patient", json=patient)
+    # r = requests.post("http://127.0.0.1:5002/api/heart_rate", json=patient)
