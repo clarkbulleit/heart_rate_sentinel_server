79f369d46e78f53c11c506fc7681e5bde6338531
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-16 17:07:45 -0500

    creates the ability to add new patients to the database and delete all previous existing ones

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import requests
 import random
+from patients import Patient
+from pymodm import connect
 
 # url = 'http://vcm-7335.vm.duke.edu:5002/'
 url = 'http://127.0.0.1:5002/'
@@ -57,31 +59,50 @@ def client_post_interval(id, cutoff_time):
 
 
 if __name__ == "__main__":
+    connect("mongodb://clarkbulleit:goduke112@ds037778.mlab.com:"
+            "37778/patients_cb329")
 
     route_type = 1
+    num_patients = 5
 
-    # tests add_new_patient route by
-    # attempting to add id numbers from
+    id = list(range(1, num_patients+1))
+
+    # tests add_new_patient route by deleting current directory and
+    # adding the specified number of patients
+
+    # Deletes all prior files in the database
+    p = Patient.objects.raw({})
+    p.delete()
+
     if route_type == 1:
-        id = list(range(1, 11))
         email = 'clarkbulleit@gmail.com'
 
-        for x in range(10):
+        for x in range(num_patients):
             r = random.randint(1, 101)
             r1 = client_add_new_patient(id[x], email, r)
             print(r1.json())
 
-    r2 = client_add_patient_hr(id, hr)
-    print(r2.json())
+    # tests the POST patient heart rate route by
+    # posting random heart rates for the patients
+    if route_type == 2:
+        for x in id:
+            print(x)
+            for h in range(num_patients):
+                hr = random.randint(45, 190)
+                r2 = client_add_patient_hr(x, hr)
+                print(r2.json())
 
-    r3 = client_get_status(id)
-    print(r3.json())
+    # Tests the GET patient status route
+    if route_type == 3:
+        for x in id:
+            r3 = client_get_status(x)
+            print(r3.json())
 
-    r4 = client_get_hr_data(id)
-    print(r4.json())
+    # r4 = client_get_hr_data(id)
+    # print(r4.json())
 
-    r5 = client_get_hr_avg(id)
-    print(r5.json())
+    # r5 = client_get_hr_avg(id)
+    # print(r5.json())
 
-    r6 = client_post_interval(id, cutoff_time)
-    print(r6.json())
+    # r6 = client_post_interval(id, cutoff_time)
+    # print(r6.json())
