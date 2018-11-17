f0979f465d402dc5bfba026713b8975154194655
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-17 11:53:22 -0500

    added docstrings to send_email function

--- first_sendgrid_email.py
+++ first_sendgrid_email.py
@@ -5,6 +5,14 @@ from sendgrid.helpers.mail import *
 
 
 def send_email(attending_email, patient_id, timestamp):
+    """ Sends email to attending_email if patient is tachycardic
+
+    :param attending_email: string
+    :param patient_id: Integer
+    :param timestamp: Time the data is input into function, string
+
+    :return: Email to attending doctor
+    """
     sg = sendgrid.SendGridAPIClient(apikey='SG.lpj1iTqMRtmDXd5GC'
                                            'xoiHQ.rXDJvAce_fNivEw'
                                            'ZMKC53uIE9Quq-ocTHAX9w'
