eacab6cd6bf14c91d22ba040dd09b8a6d954cfe0
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-17 11:58:02 -0500

    added docstrings to validate get request function

--- validate_GET_request.py
+++ validate_GET_request.py
@@ -2,6 +2,18 @@ from patients import Patient
 
 
 def validate_get_request(patient_id):
+    """ Validates inputs for all get requests in route
+
+    Function will return code that corresponds to an error
+    message.
+    0: Integer not entered
+    1: Patient doesnt exist
+    2: Heart rate list is empty
+    3: Request is valid
+
+    :param patient_id: Integer
+    :return: Code 0-3
+    """
 
     try:
         int(patient_id)
